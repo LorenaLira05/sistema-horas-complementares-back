9223ec9617b26b038e5fccfcdc4352831d365bd2
Lorena Lira <naheelee2005@gmail.com>
2026-03-30 18:13:29 -0300

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const adminController = require('../controllers/adminController'); // Importa o gerente
+const adminController = require('../controllers/adminController'); 
 
-// Agora, para ver os cursos, precisa ser SUPER_ADMIN
+// para ver os cursos, precisa ser SUPER_ADMIN
 router.get('/cursos', authMiddleware(['SUPER_ADMIN']), adminController.getListaCursos);
-// ... (mantenha o que já tem)
 router.post('/coordenador', authMiddleware(['SUPER_ADMIN']), adminController.postCadastrarCoordenador);
 router.post('/curso', authMiddleware(['SUPER_ADMIN']), adminController.postCriarCurso);
 
