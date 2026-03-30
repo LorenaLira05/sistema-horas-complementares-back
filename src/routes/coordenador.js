b5025f4e05c440ec5f454da4881a3c0d4bb90f0c
Lorena Lira <naheelee2005@gmail.com>
2026-03-30 18:14:16 -0300

    Update comments for clarity in coordenador.js

--- src/routes/coordenador.js
+++ src/routes/coordenador.js
@@ -6,16 +6,16 @@ const authMiddleware = require('../middleware/auth');
 router.get('/teste', (req, res) => res.json({ msg: "Rota Coordenador funcionando!" }));
 router.post('/regras', authMiddleware(['COORDENADOR']), coordenadorController.postCriarRegra);
 router.get('/regras/:curso_id', authMiddleware(['COORDENADOR', 'ALUNO']), coordenadorController.getRegrasPorCurso);
-// 1. Cadastrar Aluno
+// cadastrar Aluno
 router.post('/aluno', authMiddleware(['COORDENADOR']), coordenadorController.postCadastrarAluno);
 
-// 2. Ver Alunos (passando o ID do curso na URL)
+//ver Alunos (passando o ID do curso na URL)
 router.get('/alunos/:curso_id', authMiddleware(['COORDENADOR']), coordenadorController.getAlunosDoCurso);
 
-// 3. Ver Submissões Pendentes
+// ver Submissões Pendentes
 router.get('/submissoes/:curso_id', authMiddleware(['COORDENADOR']), coordenadorController.getSubmissoesPendentes);
 
-// 4. Validar Submissão
+// validar Submissão
 router.patch('/validar/:id', authMiddleware(['COORDENADOR']), coordenadorController.patchValidarSubmissao);
 
 module.exports = router;
